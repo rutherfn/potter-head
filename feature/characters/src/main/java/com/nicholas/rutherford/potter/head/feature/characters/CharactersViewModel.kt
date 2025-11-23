4d8d58c67aea0418efdd57b18806b0ddc33ebb4e
Nicholas Rutherford <nicholasrutherford@Nicholass-MBP.lan>
2025-11-23 10:00:33 -0600

    add api logic to the characters view model to be refactored in the future

--- feature/characters/src/main/java/com/nicholas/rutherford/potter/head/feature/characters/CharactersViewModel.kt
+++ feature/characters/src/main/java/com/nicholas/rutherford/potter/head/feature/characters/CharactersViewModel.kt
@@ -1,12 +1,31 @@
 package com.nicholas.rutherford.potter.head.feature.characters
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.nicholas.rutherford.potter.head.network.HarryPotterApiRepository
 import dev.zacsweers.metro.Inject
+import kotlinx.coroutines.launch
 
 class CharactersViewModel @Inject constructor(
     private val repository: HarryPotterApiRepository
 ) : ViewModel() {
+
+    init {
+        loadAllCharacters()
+    }
+
+    private fun loadAllCharacters() {
+        //todo -> More work on this its just placeholder for now
+        viewModelScope.launch {
+            repository.getAllCharacters().collect { result ->
+                result.onSuccess { characters ->
+
+                }.onFailure { error ->
+
+                }
+            }
+        }
+    }
 
     fun onCharacterClicked() {
 

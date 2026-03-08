93625a1b65f31f9a240593a544d7ee68b08486c5
Nicholas Rutherford <nicholasrutherford@Nicholass-MBP.lan>
2026-03-08 11:39:20 -0500

    clean up api layer

--- test-utils/src/test/java/com/nicholas/rutherford/potter/head/test/utils/TestHarryPotterApiServiceTest.kt
+++ test-utils/src/test/java/com/nicholas/rutherford/potter/head/test/utils/TestHarryPotterApiServiceTest.kt
@@ -3,7 +3,6 @@ package com.nicholas.rutherford.potter.head.test.utils
 import com.nicholas.rutherford.potter.head.test.utils.api.TestHarryPotterApiService
 import com.nicholas.rutherford.potter.head.test.utils.data.TestCharacterResponse
 import com.nicholas.rutherford.potter.head.test.utils.ext.shouldBe
-import com.nicholas.rutherford.potter.head.test.utils.ext.shouldBeInstanceOf
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -33,57 +32,6 @@ class TestHarryPotterApiServiceTest {
             characters.isEmpty() shouldBe true
             characters.size shouldBe 0
             characters shouldBe emptyList()
-        }
-    }
-
-    @Nested
-    inner class FetchCharacterById {
-
-        @Test
-        fun `should return character with given id when shouldReturnData is true and shouldThrowException is false`() = runTest {
-            val apiService =
-                TestHarryPotterApiService(shouldReturnData = true, shouldThrowException = false)
-            val testId = "custom-character-id"
-            val characters = apiService.fetchCharacterById(id = testId)
-
-            characters.isNotEmpty() shouldBe true
-            val character = characters.first()
-            character.id shouldBe testId
-            character.name shouldBe "Harry Potter"
-            character.house shouldBe "Gryffindor"
-        }
-
-        @Test
-        fun `should throw IllegalStateException when shouldThrowException is true`() = runTest {
-            val apiService = TestHarryPotterApiService(
-                shouldReturnData = true,
-                shouldThrowException = true
-            )
-            val testId = "test-id"
-
-            try {
-                apiService.fetchCharacterById(id = testId)
-                throw AssertionError("Expected IllegalStateException to be thrown")
-            } catch (exception: IllegalStateException) {
-                exception.shouldBeInstanceOf<IllegalStateException>()
-                exception.message shouldBe "Simulated API exception for testing error handling"
-            }
-        }
-
-        @Test
-        fun `should return character when shouldReturnData is false but shouldThrowException is false`() = runTest {
-            val apiService = TestHarryPotterApiService(
-                shouldReturnData = false,
-                shouldThrowException = false
-            )
-            val testId = "test-id"
-
-            val characters = apiService.fetchCharacterById(id = testId)
-
-            characters.isNotEmpty() shouldBe true
-            val character = characters.first()
-            character.id shouldBe testId
-            character.name shouldBe "Harry Potter"
         }
     }
 }
